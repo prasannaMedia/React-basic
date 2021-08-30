8dad830c4cfaebf8cc881ba62d620fffddab4631
prasanna <67369708+prasannaMedia@users.noreply.github.com>
2021-08-31 00:02:57 +0530

    updated

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import "./About.css";
 
 function About() {
-  return <div className="about">About page updated</div>;
+  return <div className="about">About page updated by prasanna</div>;
 }
 
 export default About;
